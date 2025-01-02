9a47160e8f43dc27897f68aed42c0fb2ad47fcb1
andrmda18 <kuliah2023telyu@gmail.com>
2025-01-02 09:32:34 +0700

    perbaikan

--- user/tarikkoin.php
+++ user/tarikkoin.php
@@ -82,7 +82,7 @@ if (isset($_POST['updateEwallet'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Penarikan Koin</title>
-    link
+    <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
